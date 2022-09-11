bae8a1d2c49195881d7bd6a59cd2dfaed7596143
Krishan Patel <krishan711@gmail.com>
2022-09-11 10:32:39 +0100

    .

---
+++ app/src/components/Toast.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { generateUUID } from '@kibalabs/core';
+import { Box } from '@kibalabs/ui-react';
+import toast, { Toaster } from 'react-hot-toast';
+
+
+export const KibaToastContainer = (): React.ReactElement => {
+  return (
+    <Toaster position='top-right' />
+  );
+};
+
+export class IKibaToastManager {
+  showToast: (variant: string, text: string, toastId?: string, shouldNotAutoClose?: boolean, autoCloseSeconds?: number) => string;
+  showCustomToast: (component: React.ReactElement, toastId?: string, shouldNotAutoClose?: boolean, autoCloseSeconds?: number) => string;
+}
+
+// NOTE(krishan711): this is at the stage where I need to create the toast component.
+// It should have a background (and text?) (and iconText?) in its styles. no state needed so I think it's a particle.
+// It should allow an icon (just like button) that can be colored (based on the variant?)
+// It should allow for either text to be passed in or another component (as children?)
+export const useToastManager = (): IKibaToastManager => {
+  const showToast = React.useCallback((variant: string, text: string, toastId?: string, shouldNotAutoClose?: boolean, autoCloseSeconds?: number): string => {
+    const actualToastId = toastId || generateUUID();
+    const shouldAutoClose = !shouldNotAutoClose;
+    const actualAutoCloseSeconds = autoCloseSeconds ?? 5000;
+    toast.custom(<Box variant='notification' isFullWidth={false}>{text}</Box>, { id: actualToastId, className: variant, duration: shouldAutoClose ? actualAutoCloseSeconds : Infinity, style: { padding: '0' } });
+    return actualToastId;
+  }, []);
+
+  const showCustomToast = React.useCallback((component: React.ReactElement, toastId?: string, shouldNotAutoClose?: boolean, autoCloseSeconds?: number): string => {
+    const actualToastId = toastId || generateUUID();
+    const shouldAutoClose = !shouldNotAutoClose;
+    const actualAutoCloseSeconds = autoCloseSeconds ?? 5000;
+    toast.custom(component, { id: actualToastId, duration: shouldAutoClose ? actualAutoCloseSeconds : Infinity, style: { padding: '0' } });
+    return actualToastId;
+  }, []);
+
+  return { showToast, showCustomToast };
+};

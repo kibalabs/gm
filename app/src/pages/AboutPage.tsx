ce59c2a7efd632af7edd66514e457999b72b7e8c
A Fox <arthur.fox@hotmail.co.uk>
2022-09-26 13:59:26 +0100

    Fiddle: Updated AboutPage text (#16)
    Updated to introduce this idea of getting WL, added a link to the spreadsheet with winners, and tweaked a few other parts.

--- app/src/pages/AboutPage.tsx
+++ app/src/pages/AboutPage.tsx
@@ -3,37 +3,41 @@ import React from 'react';
 import { Alignment, Direction, Markdown, PaddingSize, Spacing, Stack, Text, TextAlignment } from '@kibalabs/ui-react';
 
 const CONTENT = `
-**Communities that GM together, stay together**
+### Communities that GM together, stay together
 
-At [Token Page](https://www.tokenpage.xyz), we absolutely love when people say GM - it really makes our day! We realized over the last year that the communities that GM together stay together, and we found ourselves having more fun in these communities too.
+At [Token Page](https://www.tokenpage.xyz), we absolutely love when people say GM - it really makes our day.
 
-So we got together with [@confuseddegen](https://twitter.com/confuseddegen) and built GM. It's super simple, just visit the page, connect your wallet and GM.
+We realized over the last year that the communities that GM together stay together, and we found ourselves having more fun in these communities too. So we built GM!
+
+It's super simple, just visit the page, connect your wallet and GM - that's it! 😄
 
 *But, why?*
 
-Well, if you GM you're signalling to the world that you're still here and are in web3 for the vibes. Plus, there are awesome prizes to be won!
+Well, if you GM you're signalling to the world that you're still here and are in web3 for the vibes. Plus, you stand to win prizes!
 
-### The prizes
+### The prizes 🎁
 
-Every day, one GM-er from the collection with the most GMs will win an NFT from our GM NFT collection.
+Every day 1 GM-er from the collection with the most GMs will win 1 WL spot in an upcoming NFT project!
 
-Holders of the GM tokens, will be exclusive access to [REDACTED]! (we're not quite ready to share yet, but keep your eyes peeled 👀)
+Keep your daily streak of GMs going for long enough and you will be in for a chance to win [REDACTED] too. 😉
 
-Our GM tokens will be produced once a day and will be tradeable like all other NFTs. The artwork will be revealed in October and we are very excited to show it to you. It's going to be a generative PFP collection with some very unique features!
+More new prizes will be shared over the coming months! 👀
 
-### How to win
+You can see all the past daily winners [here](https://bit.ly/3dEGSaI).
 
-This is the easy part! When ever you GM (once a day max) you add a point to the daily score of each community you hold a token for.
+### How do I win? 🤔
 
-To get your community to the top you need to get everyone holding the token to come to the page and GM.
+This is the easy part! Whenever you GM, once per day, you add a point to the daily score of each community you hold an NFT for.
 
-If your community is at the top of the list at the end of the day (in the UTC timezone), one of the GM-ers from that collection will win the GM NFT 🙌
+Get your community to the top by getting others holding that NFT to GM too!
 
-So the trick is: just get everyone from your community to come to the page and GM every day. If you are in many communities your best bet is to share it with all of them so that personally you are very likely to be in one of the communities that wins, thus increasing your odds of winning!
+The collection at the top of the list at the end of the day (in the UTC timezone) will have one random collector win the prize of the day. 🙌
 
-If you have questions, feel free to reach out to us on twitter at [@tokenpagexyz](https://twitter.com/tokenpagexyz) 💬
+So the trick is: just get everyone from your community to come to the page and GM every day! If you are in many communities your best bet is to share it with all of them so that you raise your personal chances of being in the community that wins!
 
-**Good luck and GM frens!**
+If you have questions, just send us a tweet to [@tokenpagexyz](https://twitter.com/tokenpagexyz) 💬
+
+**Good luck and GM frens!** 👋
 `;
 
 export const AboutPage = (): React.ReactElement => {

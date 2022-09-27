183c784d7b185bbc0b7830152777a47cea49e2fc
A Fox <arthur.fox@hotmail.co.uk>
2022-09-27 21:45:18 +0100

    Removed name of WL (#18)

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -176,7 +176,7 @@ export const HomePage = (): React.ReactElement => {
         <Stack direction={Direction.Vertical} isFullHeight={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true} paddingTop={PaddingSize.Wide} paddingBottom={PaddingSize.Wide} paddingHorizontal={PaddingSize.Wide}>
           <Box variant='topBar' isFullWidth={true} height={'2em'} shouldClipContent={true}>
             <Stack direction={Direction.Vertical} shouldAddGutters={true}>
-              <Text alignment={TextAlignment.Center}>🎁 Prize today is 1 WL spot @privesociete</Text>
+              <Text alignment={TextAlignment.Center}>🎁 Prize today is 1 WL spot!</Text>
             </Stack>
           </Box>
           <Spacing />
